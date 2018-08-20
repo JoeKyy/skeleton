0a281ae2738f722fa7e3601f50d6404c37753282
Jhomar Nando <contato@joekyy.com.br>
2018-08-20 01:19:56 -0300

    Adding the skeleton stucture for init projects

---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+var gulp        =  require('gulp')
+    install     =  require("gulp-install")
+    sass        =  require('gulp-sass')
+    browserSync =  require('browser-sync')
+    useref      =  require('gulp-useref')
+    concat      =  require('gulp-concat')
+    uglify      =  require('gulp-uglify')
+    uglifycss   =  require('gulp-uglifycss')
+    gulpIf      =  require('gulp-if')
+    imagemin    =  require('gulp-imagemin')
+    cache       =  require('gulp-cache')
+    del         =  require('del')
+    spritesmith =  require('gulp.spritesmith')
+    runSequence =  require('run-sequence');
+
+gulp.task('install', function () {
+   return gulp.src(['./bower.json', './package.json'])
+  .pipe(install());
+});
+
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({'uglyComments': true}))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('sprite', function () {
+  var spriteData =
+    gulp.src('./app/images/sprites/*.*')
+      .pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: '_sprite.sass',
+        cssFormat: 'sass',
+        algorithm: 'binary-tree',
+        cssTemplate: 'sass.template.mustache',
+        cssVarMap: function (sprite) {
+          sprite.name = sprite.name
+        }
+      }));
+
+  spriteData.img.pipe(gulp.dest('./app/images/'));
+  spriteData.css.pipe(gulp.dest('./app/scss/'));
+
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/imgs/sprites/*.*', ['sprite']);
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/scss/**/**/*.scss', ['sass'])
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+gulp.task('useref', function () {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('app/css/*.css')
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('images', function () {
+  return gulp
+    .src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(
+      cache(
+        imagemin({
+          interlaced: true
+        })
+      )
+    )
+    .pipe(gulp.dest('dist/images'))
+})
+
+
+gulp.task('fonts', function () {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('clean', function () {
+  return del.sync('dist').then(function (cb) {
+    return cache.clearAll(cb);
+  });
+})
+
+gulp.task('clean:dist', function () {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass', 'browserSync'], 'watch',
+    callback
+  )
+})
+
+gulp.task('build', function (callback) {
+  runSequence(
+    'clean:dist',
+    'sass',
+    ['useref', 'images', 'css', 'fonts'],
+    callback
+  )
+})
+
+gulp.task('run', function (callback) {
+  runSequence('install', callback)
+})
